b87c17a4c32041b8151eeceac3cf513a497b08a0
Hiram Muñoz <hirammunozr@outlook.com>
2023-12-13 01:53:15 -0600

    changed clarity script again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import React, {type ReactNode} from 'react';
 import {type Metadata} from 'next';
 import 'leaflet/dist/leaflet.css';
-import Head from 'next/head';
+import Script from 'next/script';
 import './globals.css';
 import ClientProviders from '@/app/client-providers.tsx';
 import {ToastProvider} from '@/components/toast.tsx';
@@ -19,17 +19,16 @@ export default function RootLayout(props: RootLayoutProps) {
 	const {children} = props;
 	return (
 		<html lang='en'>
-			<Head>
-				<script type='text/javascript'>
-					{`
+			<Script
+				id='clarity-script' dangerouslySetInnerHTML={{
+					__html: `
           (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "jh8ea3tii2");
-        `}
-				</script>
-			</Head>
+        `,
+				}}/>
 			<body className='bg-stone-950'>
 				<ClientProviders>
 					<ToastProvider>
