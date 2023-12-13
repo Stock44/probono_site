17e3bc480f1d684ea47e4eeba5095b57be7c7ae8
Hiram Muñoz <hirammunozr@outlook.com>
2023-12-13 01:43:14 -0600

    changed clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Script from 'next/script';
 import './globals.css';
 import ClientProviders from '@/app/client-providers.tsx';
 import {ToastProvider} from '@/components/toast.tsx';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
 	title: 'probono',
@@ -19,15 +20,17 @@ export default function RootLayout(props: RootLayoutProps) {
 	const {children} = props;
 	return (
 		<html lang='en'>
-			<Script strategy='lazyOnload' id='clarity-script'>
-				{`
+			<Head>
+				<script type='text/javascript'>
+					{`
           (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "jh8ea3tii2");
         `}
-			</Script>
+				</script>
+			</Head>
 			<body className='bg-stone-950'>
 				<ClientProviders>
 					<ToastProvider>
