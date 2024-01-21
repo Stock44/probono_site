097e14cbca1a7e1e614da748cfb49c776765d66d
Samantha <samanthasofiabg@gmail.com>
2024-01-20 19:58:35 -0600

    npx xo fix

--- src/components/tooltip-button-trigger.tsx
+++ src/components/tooltip-button-trigger.tsx
@@ -1,36 +1,36 @@
-import React, { ForwardedRef, forwardRef } from 'react';
-import { mergeProps, useTooltipTrigger, type TooltipTriggerProps } from 'react-aria';
-import { useTooltipTriggerState } from 'react-stately';
-import Button, { type ButtonProps } from './button.tsx';
-import Tooltip from './tooltip';
-import { useObjectRef } from '@react-aria/utils';
+import {useObjectRef} from '@react-aria/utils';
+import React, {forwardRef, type ForwardedRef} from 'react';
+import {mergeProps, useTooltipTrigger, type TooltipTriggerProps} from 'react-aria';
+import {useTooltipTriggerState} from 'react-stately';
+import Button, {type ButtonProps} from './button.tsx';
+import Tooltip from './tooltip.tsx';
 
 type TooltipButtonProps = {
-  tooltipTriggerProps: TooltipTriggerProps,
-  children: React.ReactNode,
-  tooltip: React.ReactNode,
+	readonly tooltipTriggerProps: TooltipTriggerProps;
+	readonly children: React.ReactNode;
+	readonly tooltip: React.ReactNode;
 } & ButtonProps;
 
 const TooltipButton = forwardRef((props: TooltipButtonProps, ref: ForwardedRef<HTMLButtonElement>) => {
-  const { tooltipTriggerProps, children, tooltip } = props;
-  const state = useTooltipTriggerState(tooltipTriggerProps);
-  const buttonRef = useObjectRef(ref);
+	const {tooltipTriggerProps, children, tooltip} = props;
+	const state = useTooltipTriggerState(tooltipTriggerProps);
+	const buttonRef = useObjectRef(ref);
 
-  const { triggerProps, tooltipProps } = useTooltipTrigger(tooltipTriggerProps, state, buttonRef);
+	const {triggerProps, tooltipProps} = useTooltipTrigger(tooltipTriggerProps, state, buttonRef);
 
-  return (
-    <span style={{ position: 'relative' }}>
-      <Button
-        {...mergeProps(triggerProps, props)}
-        ref={ref}
-      >
-        {children}
-      </Button>
-      {state.isOpen && (
-        <Tooltip state={state} ariaTooltipProps={tooltipProps}>{tooltip}</Tooltip>
-      )}
-    </span>
-  );
+	return (
+		<span style={{position: 'relative'}}>
+			<Button
+				{...mergeProps(triggerProps, props)}
+				ref={ref}
+			>
+				{children}
+			</Button>
+			{state.isOpen && (
+				<Tooltip state={state} ariaTooltipProps={tooltipProps}>{tooltip}</Tooltip>
+			)}
+		</span>
+	);
 });
 
 export default TooltipButton;
--- src/components/tooltip.tsx
+++ src/components/tooltip.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
-import { mergeProps, useTooltip, type AriaTooltipProps } from 'react-aria';
-import type { TooltipTriggerState } from 'react-stately';
+import {mergeProps, useTooltip, type AriaTooltipProps} from 'react-aria';
+import type {TooltipTriggerState} from 'react-stately';
 
 type TooltipProps = {
-	ariaTooltipProps: AriaTooltipProps, 
-	children: React.ReactNode,
-	state?: TooltipTriggerState
-}
+	readonly ariaTooltipProps: AriaTooltipProps;
+	readonly children: React.ReactNode;
+	readonly state?: TooltipTriggerState;
+};
 
-const Tooltip = (props: TooltipProps) => {
-	const { ariaTooltipProps, state, children } = props
-	const { tooltipProps } = useTooltip(ariaTooltipProps, state);
+function Tooltip(props: TooltipProps) {
+	const {ariaTooltipProps, state, children} = props;
+	const {tooltipProps} = useTooltip(ariaTooltipProps, state);
 
 	return (
-	<span
-		style={{
-		position: 'absolute',
-		left: '5px',
-		top: '100%',
-		maxWidth: 150,
-		marginTop: '10px',
-		backgroundColor: 'white',
-		color: 'black',
-		padding: '5px',
-		border: '1px solid gray'
-		}}
-		{...mergeProps(ariaTooltipProps, tooltipProps)}
-	>
-		{children}
-	</span>
+		<span
+			style={{
+				position: 'absolute',
+				left: '5px',
+				top: '100%',
+				maxWidth: 150,
+				marginTop: '10px',
+				backgroundColor: 'white',
+				color: 'black',
+				padding: '5px',
+				border: '1px solid gray',
+			}}
+			{...mergeProps(ariaTooltipProps, tooltipProps)}
+		>
+			{children}
+		</span>
 	);
 }
 
