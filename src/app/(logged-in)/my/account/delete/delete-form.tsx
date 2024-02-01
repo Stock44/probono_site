86c0dbf4473eea72c6cc3e9ede4c156b8e8c6f0f
IsidroTrevino <A00837104@tec.mx>
2024-01-31 18:48:31 -0600

    Reformat code for delete page and form and user tests

--- src/app/(logged-in)/my/account/delete/delete-form.tsx
+++ src/app/(logged-in)/my/account/delete/delete-form.tsx
@@ -1,37 +1,26 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import Save from "@material-design-icons/svg/round/save.svg";
-import { type User } from "@prisma/client";
-import { type UserDelete, userDeleteSchema } from "@/lib/schemas/user.ts";
-import Form, { type FormState } from "@/components/form.tsx";
-import SubmitButton from "@/components/submit-button.tsx";
-import TextField from "@/components/text-field.tsx";
-import { formValidators } from "@/lib/form-utils.ts";
-import { getSession } from "@auth0/nextjs-auth0";
+import React, {useState} from 'react';
+import Save from '@material-design-icons/svg/round/save.svg';
+import {type User} from '@prisma/client';
+import {type UserDelete, userDeleteSchema} from '@/lib/schemas/user.ts';
+import Form, {type FormState} from '@/components/form.tsx';
+import SubmitButton from '@/components/submit-button.tsx';
+import TextField from '@/components/text-field.tsx';
+import {formValidators} from '@/lib/form-utils.ts';
 
 export type PasswordFormProps = {
-  readonly action: (
-    state: FormState<UserDelete>,
-    data: FormData,
-  ) => Promise<FormState<UserDelete>>;
+	readonly action: (state: FormState<UserDelete>, data: FormData) => Promise<FormState<UserDelete>>;
 };
 
 export default function DeleteForm(props: PasswordFormProps) {
-  const { action } = props;
-  const validate = formValidators(userDeleteSchema);
+	const {action} = props;
+	const validate = formValidators(userDeleteSchema);
 
-  return (
-    <Form action={action}>
-      <TextField
-        isRequired
-        validate={validate.password}
-        label="Ingrese su contraseña actual"
-        name="password"
-        type="password"
-        className="mb-4"
-      />
-      <SubmitButton icon={<Save />}>Eliminar cuenta</SubmitButton>
-    </Form>
-  );
+	return (
+		<Form action={action}>
+			<TextField isRequired validate={validate.password} label='Ingrese su contraseña actual' name='password' type='password' className='mb-4'/>
+			<SubmitButton icon={<Save/>}>Eliminar cuenta</SubmitButton>
+		</Form>
+	);
 }
--- src/app/(logged-in)/my/account/delete/page.tsx
+++ src/app/(logged-in)/my/account/delete/page.tsx
@@ -1,53 +1,42 @@
-import React from "react";
-import { redirect } from "next/navigation";
-import { getSession } from "@auth0/nextjs-auth0";
-import Deleteform from "./delete-form";
-("@/app/(logged-in)/my/account/password/password-form.tsx");
-import { type UserDelete, userDeleteSchema } from "@/lib/schemas/user";
-import { type FormState } from "@/components/form.tsx";
-import { decodeForm } from "@/lib/form-utils.ts";
-import { handleActionError } from "@/lib/handle-action-error.ts";
-import { authentication } from "@/lib/auth0.ts";
-import { deleteUser } from "@/lib/models/user.ts";
+import React from 'react';
+import {redirect} from 'next/navigation';
+import {getSession} from '@auth0/nextjs-auth0';
+import Deleteform from './delete-form.tsx';
+import {type UserDelete, userDeleteSchema} from '@/lib/schemas/user.ts';
+import {type FormState} from '@/components/form.tsx';
+import {decodeForm} from '@/lib/form-utils.ts';
+import {handleActionError} from '@/lib/handle-action-error.ts';
+import {authentication} from '@/lib/auth0.ts';
+import {deleteUser} from '@/lib/models/user.ts';
 
 export default async function AccountPage() {
-  const session = await getSession();
-  if (
-    !session ||
-    !session.user ||
-    !session.user.sub ||
-    !session.user.sub.includes("auth0")
-  ) {
-    redirect("/my/account");
-  }
+	const session = await getSession();
+	if (!session || !session.user || !session.user.sub || !session.user.sub.includes('auth0')) {
+		redirect('/my/account');
+	}
 
-  const action = async (
-    state: FormState<UserDelete>,
-    data: FormData,
-  ): Promise<FormState<UserDelete>> => {
-    "use server";
-    try {
-      const parsedData = await decodeForm(data, userDeleteSchema);
+	const action = async (state: FormState<UserDelete>, data: FormData): Promise<FormState<UserDelete>> => {
+		'use server';
+		try {
+			const parsedData = await decodeForm(data, userDeleteSchema);
+			await authentication.oauth.passwordGrant({
+				username: session.user?.email as string,
+				password: parsedData.password,
+			});
 
-      await authentication.oauth.passwordGrant({
-        username: session.user.email as string,
-        password: parsedData.password,
-      });
+			await deleteUser(session.user.sub as string);
+		} catch (error) {
+			console.log(error);
+			return handleActionError(state, error);
+		}
 
-      await deleteUser(session.user.sub);
-    } catch (error) {
-      console.log(session.user);
-      console.log(error);
-      return handleActionError(state, error);
-    }
+		redirect('/');
+	};
 
-    redirect("/");
-  };
-
-  return (
-    <main>
-      <h1 className="text-stone-200 text-4xl mb-2">Eliminacion de cuenta</h1>
-      <Deleteform action={action} />
-    </main>
-  );
+	return (
+		<main>
+			<h1 className='text-stone-200 text-4xl mb-2'>Eliminacion de cuenta</h1>
+			<Deleteform action={action}/>
+		</main>
+	);
 }
