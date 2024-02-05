b56cc1b92cd833d74c25463389ccdee644bdb35c
Hiram Muñoz <hirammunozr@outlook.com>
2024-02-05 17:02:27 -0600

    Simplify `LoggedInLayout` by removing extraneous imports and method calls
    This commit streamlines the code for the `LoggedInLayout` component, reducing its complexity by removing a number of unnecessary imports and method calls. Now, the component solely returns the children props, concentrating solely on the fundamental responsibility of the component: ensuring the user is logged in.

--- src/app/(logged-in)/layout.tsx
+++ src/app/(logged-in)/layout.tsx
@@ -1,41 +1,16 @@
 import React, {type ReactNode} from 'react';
 import {withPageAuthRequired} from '@auth0/nextjs-auth0';
-import {notFound, useSearchParams} from 'next/navigation';
-import {cookies} from 'next/headers';
-import {Organization} from '@prisma/client';
-import invariant from 'ts-tiny-invariant';
-import TopBarFooterLayout from '@/components/top-bar-footer-layout.tsx';
-import AccountButton from '@/app/(logged-in)/onboarding/account-button.tsx';
-import {
-	getCurrentUserOrganizations, getUsersActiveOrganization,
-} from '@/lib/models/user.ts';
-import OrganizationSelectorButton from '@/components/organization-selector/organization-selector-button.tsx';
 
 export type LoggedInLayoutProps = {
 	readonly children: ReactNode;
 };
 
+// Ensures that for all routes within the (logged-in) group, the user is logged in
 // @ts-expect-error withPageAuthRequired typings are too strict
 const LoggedInLayout = withPageAuthRequired(async (props: LoggedInLayoutProps) => {
 	const {children} = props;
 
-	const organizations = await getCurrentUserOrganizations();
-
-	const organization = await getUsersActiveOrganization();
-
-	return (
-		<TopBarFooterLayout topBarItems={
-			<>
-				{
-					organizations && organizations.length > 0
-            && <OrganizationSelectorButton items={organizations} currentOrganization={organization!}/>
-				}
-				<AccountButton/>
-			</>
-		}>
-			{children}
-		</TopBarFooterLayout>
-	);
+	return children;
 }, {
 	returnTo: '/my',
 });
