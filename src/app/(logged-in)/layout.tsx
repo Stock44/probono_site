e1706f910e2c8e8aaceb5fe6154d137243401d2c
Hiram Muñoz <hirammunozr@outlook.com>
2024-02-05 18:42:17 -0600

    Refactor LoggedInLayout component for session verification
    This commit refactors the LoggedInLayout component to use 'getSession' for user session verification instead of 'withPageAuthRequired'. Additionally, it adds a 'redirect' function to navigate non-authenticated users to the login page. Also, the 'children' prop return type has been enforced to JSX.Element to prevent possible type compilation issues.

--- src/app/(logged-in)/layout.tsx
+++ src/app/(logged-in)/layout.tsx
@@ -1,18 +1,21 @@
 import React, {type ReactNode} from 'react';
-import {withPageAuthRequired} from '@auth0/nextjs-auth0';
+import {getSession} from '@auth0/nextjs-auth0';
+import {redirect} from 'next/navigation';
 
 export type LoggedInLayoutProps = {
 	readonly children: ReactNode;
 };
 
 // Ensures that for all routes within the (logged-in) group, the user is logged in
-// @ts-expect-error withPageAuthRequired typings are too strict
-const LoggedInLayout = withPageAuthRequired(async (props: LoggedInLayoutProps) => {
+export default async function LoggedInLayout(props: LoggedInLayoutProps) {
 	const {children} = props;
 
-	return children;
-}, {
-	returnTo: '/my',
-});
+	const session = await getSession();
 
-export default LoggedInLayout;
+	if (!session) {
+		redirect('/');
+	}
+
+	// Solves type compilation issue
+	return children as JSX.Element;
+}
