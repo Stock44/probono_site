66376a0896a8a662fb2c8d229245008c9c321694
Hiram Muñoz <hirammunozr@outlook.com>
2024-02-06 21:48:32 -0600

    Implement dynamic top bar and update onboarding layout
    Added a new dynamic behavior to the top bar that changes its visibility based on the user's scroll position. Also updated the layout for the onboarding process for logged-in users, where the sign-out option is now added to the dynamic top bar. Additionally, there's an enhancement with a new library 'use-scroll-position' that is added to make the change feasible and efficient.

--- src/app/(logged-in)/onboarding/layout.tsx
+++ src/app/(logged-in)/onboarding/layout.tsx
@@ -1,7 +1,12 @@
 import React, {type ReactNode} from 'react';
 import {redirect} from 'next/navigation';
+import Menu from '@material-design-icons/svg/round/menu.svg';
 import {getUserFromSession} from '@/lib/models/user.ts';
 import OnboardingClientLayout from '@/app/(logged-in)/onboarding/onboarding-client-layout.tsx';
+import TopBar from '@/components/top-bar.tsx';
+import SidebarTrigger from '@/components/sidebar-trigger.tsx';
+import LinkButton from '@/components/link-button.tsx';
+import Footer from '@/components/footer.tsx';
 
 export type OnboardingLayoutProps = {
 	readonly children: ReactNode;
@@ -16,8 +21,20 @@ export default async function OnboardingLayout(props: OnboardingLayoutProps) {
 	}
 
 	return (
-		<OnboardingClientLayout isOrganizationTabDisabled={!user}>
-			{children}
-		</OnboardingClientLayout>
+		<div>
+			<TopBar>
+				<SidebarTrigger icon={<Menu className='fill-current'/>} variant='text'>
+					<div className='p-4'>
+						<LinkButton href='/api/auth/logout'>Cerrar sesión</LinkButton>
+					</div>
+				</SidebarTrigger>
+			</TopBar>
+			<div className='min-h-[calc(100vh-theme(spacing.16))] md:pt-16 mt-16'>
+				<OnboardingClientLayout isOrganizationTabDisabled={!user}>
+					{children}
+				</OnboardingClientLayout>
+			</div>
+			<Footer/>
+		</div>
 	);
 }
--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,7 +1,11 @@
-import React, {type ReactNode} from 'react';
+'use client';
+import React, {type ReactNode, useState} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import GeoStatsLogoMark from 'public/logos/geostats-logomark.png';
+import {useScrollPosition} from '@n8tb1t/use-scroll-position';
+import {motion} from 'framer-motion';
+import {cx} from '@/lib/cva.ts';
 
 export type TopBarProps = {
 	readonly children: ReactNode;
@@ -10,8 +14,28 @@ export type TopBarProps = {
 export default function TopBar(props: TopBarProps) {
 	const {children} = props;
 
+	const [showBar, setShowBar] = useState(false);
+
+	useScrollPosition(({prevPos, currPos}) => {
+		console.log(currPos.y);
+		if (currPos.y === 0 && !showBar) {
+			setShowBar(true);
+		} else if (prevPos.y < currPos.y && !showBar) {
+			setShowBar(true);
+		} else if (prevPos.y > currPos.y && showBar) {
+			setShowBar(false);
+		}
+	}, [showBar]);
+
 	return (
-		<header className='w-full h-16 border-b border-stone-800 px-4'>
+		<motion.header
+			className={cx(
+				'fixed z-[1000] backdrop-blur right-0 left-0 w-full h-16 border-b border-stone-800 px-4',
+			)}
+			animate={{
+				top: showBar ? 0 : -64,
+			}}
+		>
 			<div className='max-w-7xl mx-auto h-full justify-between items-center flex gap-2'>
 				<Link href='/' className='flex items-center gap-2 text-md font-bold text-stone-50'>
 					<Image
@@ -25,7 +49,7 @@ export default function TopBar(props: TopBarProps) {
 				<div className='grow'/>
 				{children}
 			</div>
-		</header>
+		</motion.header>
 	);
 }
 
