b6cc225591fa5870e69c5674ce28611049c3b706
Hiram Muñoz <hirammunozr@outlook.com>
2024-03-11 00:31:54 -0600

    Add production and development environment variables
    This commit introduces .env.development and .env.production files for setting different values of NEXT_PUBLIC_SITE_URL. The usage of these environment variables in src/emails/organization-invitation.tsx is also updated. Some unnecessary environment-related entries are removed from .gitignore, and the .prettierignore file is updated to ignore .env.development file instead of .env.local.

--- src/emails/organization-invitation.tsx
+++ src/emails/organization-invitation.tsx
@@ -14,7 +14,7 @@ import {
 	Img, Preview,
 } from '@react-email/components';
 
-export const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
+export const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 
 export type OrganizationInvitationEmailProps = {
 	readonly organizationLogoUrl: string;
