e4b743ab2ee330c925167f6fa20867f51885918e
Hiram Muñoz <hirammunozr@outlook.com>
2024-03-13 00:02:11 -0600

    Remove redundant SQL insert code
    The commit primarily removes a considerable amount of SQL insertion code that was previously uploaded. These removed pieces of code invoked multiple insertions into multiple tables including ServiceCategory, ActivityCategory, and GovernmentOrganizationCategory, among others. This change should enhance the readability and maintenance of the overall codebase, creating more space for important and necessary attachments.

--- src/app/(logged-in)/my/page.tsx
+++ src/app/(logged-in)/my/page.tsx
@@ -13,7 +13,10 @@ import {getAddress} from '@/lib/models/address.ts';
 import DashboardTile from '@/app/(logged-in)/my/dashboard-tile.tsx';
 import SectorsList from '@/app/(logged-in)/my/sectors-list.tsx';
 import MembersList from '@/app/(logged-in)/my/members-list.tsx';
+<<<<<<< HEAD
 import Paper from '@/components/paper/paper.tsx';
+=======
+>>>>>>> a48affe (Remove redundant SQL insert code)
 
 const LocationMap = dynamic(async () => import('@/app/(logged-in)/my/location-map.tsx'), {
 	ssr: false,
@@ -39,6 +42,11 @@ async function countNullModelAttributes(model: Record<string, unknown> & {
 		}
 	}
 
+<<<<<<< HEAD
+=======
+	console.log(model);
+
+>>>>>>> a48affe (Remove redundant SQL insert code)
 	for (const value of Object.values(omit(model, ['_count']))) {
 		total++;
 		if (value === null) {
@@ -105,9 +113,14 @@ export default async function MyStartPage() {
 	return (
 		<main className='w-full'>
 			<div className='text-stone-300 w-full grid gap-4 grid-cols-1 md:grid-cols-3'>
+<<<<<<< HEAD
 				<Paper
 					hoverEffect
 					className='md:col-span-3 flex gap-8 items-center flex-wrap md:flex-nowrap justify-center md:justify-start '>
+=======
+				<div
+					className='border border-stone-800 p-8 rounded md:col-span-3 flex gap-8 items-center flex-wrap justify-center md:justify-start'>
+>>>>>>> a48affe (Remove redundant SQL insert code)
 					{
 						organization.logoUrl && (
 							<Image src={organization.logoUrl} alt='Organization logo' width={64} height={64}/>
@@ -135,6 +148,22 @@ export default async function MyStartPage() {
 					</div>
 					<div className='basis-5/12 md:basis-auto flex-none'>
 						<h3 className='text-sm text-stone-400 text-center md:text-left'>
+							Miembro(s)
+						</h3>
+						<p className='text-2xl font-bold text-center md:text-left'>
+							{organization.owners.length}
+						</p>
+					</div>
+					<div className='basis-5/12 md:basis-auto'>
+						<h3 className='text-sm text-stone-400 text-center md:text-left'>
+							Campos llenados
+						</h3>
+						<p className='text-2xl font-bold text-center md:text-left'>
+							{totals - nulls} / {totals}
+						</p>
+					</div>
+					<div className='basis-5/12 md:basis-auto'>
+						<h3 className='text-sm text-stone-400 text-center md:text-left'>
 							Estatus de aprobación
 						</h3>
 						<p className='text-2xl font-bold text-center md:text-left'>
@@ -146,7 +175,11 @@ export default async function MyStartPage() {
 						</p>
 					</div>
 
+<<<<<<< HEAD
 				</Paper>
+=======
+				</div>
+>>>>>>> a48affe (Remove redundant SQL insert code)
 				<DashboardTile title='Tu información general' href='/my/general' icon={<Feed className='fill-current'/>}>
 					<h3 className='text-xs text-stone-400 text-center md:text-left'>
 						Campos llenados
