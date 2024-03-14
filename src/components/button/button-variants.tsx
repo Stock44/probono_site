b2283fbfb4c9cffd35737a67755e91ee1dc87e47
Hiram Muñoz <hirammunozr@outlook.com>
2024-03-13 23:51:31 -0600

    Update button variant and add mouse move event to OrganizationCard
    The button variants now include 'transition-colors'. For the OrganizationCard component, additional React hooks were introduced to capture and respond to the mouse movements. The tracking of mouse positions will be used for better interactive experiences in future. Also, modifications were made to the element's CSS class to add a hover effect.

--- src/components/button/button-variants.tsx
+++ src/components/button/button-variants.tsx
@@ -2,7 +2,7 @@ import {type VariantProps} from 'cva';
 import {cva} from '@/lib/cva.ts';
 
 const buttonVariants = cva({
-	base: 'flex gap-1 items-center truncate rounded disabled:cursor-default font-bold fill-current h-fit',
+	base: 'transition-colors flex gap-1 items-center truncate rounded disabled:cursor-default w-fit font-bold fill-current h-fit',
 	variants: {
 		size: {
 			xs: 'text-xs',
