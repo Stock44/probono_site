37fe6dc8524e7ee1cd0d42b478b4f05e473b6602
Hiram Muñoz <hirammunozr@outlook.com>
2024-03-14 23:04:41 -0600

    Fixed build issue with types on organizations api route

--- src/app/api/organizations/route.ts
+++ src/app/api/organizations/route.ts
@@ -1,9 +1,8 @@
-import {type NextApiHandler} from 'next';
-import {NextResponse} from 'next/server';
+import {type NextRequest, NextResponse} from 'next/server';
 import {getApprovedOrganizationInfo} from '@/lib/models/organization.ts';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const GET: NextApiHandler = async (request, response) => {
+export const GET = async (request: NextRequest) => {
 	const organization = await getApprovedOrganizationInfo();
 
 	return NextResponse.json(organization);
