08e11c28583692f2df9ec94635753daa30e82a54
RICTEL04 <110928090+RICTEL04@users.noreply.github.com>
2024-04-13 16:22:12 -0600

    Se actualizo el correo de invitacion a la organizacion por un fondo blanco y temas oscuros y se realizo un correo de bienvenida

--- src/emails/welcome.tsx
+++ src/emails/welcome.tsx
@@ -17,7 +17,6 @@ import {
 export const baseUrl = process.env.NEXT_PUBLIC_SITE_URL!;
 
 export type OrganizationInvitationEmailProps = {
-	readonly organizationLogoUrl: string;
 	readonly organizationLogoUrl2: string;
 	readonly organizationName: string;
 	readonly senderName: string;
@@ -26,14 +25,14 @@ export type OrganizationInvitationEmailProps = {
 };
 
 export default function OrganizationInvitationEmail(props: OrganizationInvitationEmailProps) {
-	const {organizationName, senderName, senderEmail, organizationLogoUrl, organizationLogoUrl2, inviteId} = props;
+	const {organizationName, senderName, senderEmail, organizationLogoUrl2, inviteId} = props;
 	return (
 		<Html>
 			<Head>
 				<Font fontFamily='Sans' fallbackFontFamily='Helvetica'/>
 			</Head>
 			<Preview>
-				Has sido invitado a unirte a {organizationName}.
+				Bienvenido a <br/> {organizationName}.
 			</Preview>
 			<Tailwind>
 				<Body className='bg-stone-950 text-stone-300'>
@@ -41,29 +40,18 @@ export default function OrganizationInvitationEmail(props: OrganizationInvitatio
 						<Section className='bg-white border border-solid rounded border-stone-300 p-8'>
 							<Img src={organizationLogoUrl2} width={64} className='mx-auto mb-5'/>
 							<Heading className='text-black font-normal text-2xl text-center max-w-sm mb-12'>
-								Has sido invitado a unirte a <strong>{organizationName}</strong>.
+								Bienvenido a<br/><strong>{organizationName}</strong>.
 							</Heading>
 							<Text className='text-black mb-12'>
-								<strong>{senderName}</strong> (<Link href={`mailto:${senderEmail}`}>{senderEmail}</Link>)
-								te ha invitado a unirte a la plataforma <strong>[GeoStats] Pro Bono</strong> como colaborador de la
-								organización: <strong>{organizationName}</strong>.
-								<br/>
-								<br/>
-								Al unirte, podrás modificar la información de <strong>{organizationName}</strong>, ayudando a completar el perfil
-								de la organización dentro de la plataforma.
+								 Te damos la bienvenida a <strong>{organizationName}</strong>, te invitamos a seguir colaborando en el sitio <strong>[GeoStats] Pro Bono</strong> donde 
+                                 podras completar y actualizar el perfil de tu organización.
+                                 <br/>
+                                 <br/>
+                                 Por un apoyo mayor a la comunidad.
 							</Text>
-							<Img src={organizationLogoUrl} width={128} className='mx-auto'/>
 							<Text className='text-black text-center text-base font-bold mb-5'>
-								{organizationName}
+								<strong>{organizationName}</strong>
 							</Text>
-							<Section className='text-center mb-12'>
-								<Button
-									className='p-2 text-sm bg-stone-950 text-white rounded font-bold mx-auto'
-									target='_blank'
-									href={`${baseUrl}/invite/${inviteId}`}>
-									Únirme ahora
-								</Button>
-							</Section>
 						</Section>
 						<Text className='text-xs text-black'>
 							¿No quieres recibir más correos de <strong>GeoStats Pro Bono</strong>? Haz click <Link>aquí</Link> para
@@ -79,7 +67,6 @@ export default function OrganizationInvitationEmail(props: OrganizationInvitatio
 }
 
 OrganizationInvitationEmail.PreviewProps = {
-	organizationLogoUrl: 'http://localhost:3000/logos/geostats_black.png',
 	organizationLogoUrl2: 'http://localhost:3000/logos/geostats-logomark-black.png',
 	inviteId: 'aaa',
 	organizationName: 'GeoStats',
