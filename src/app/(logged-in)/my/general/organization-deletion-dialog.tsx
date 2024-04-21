69b4f97436c190c04c8c34126893f90398708fb9
IsidroTrevino <A00837104@tec.mx>
2024-04-20 20:15:23 -0600

    added the implementation for organization deletion

--- src/app/(logged-in)/my/general/organization-deletion-dialog.tsx
+++ src/app/(logged-in)/my/general/organization-deletion-dialog.tsx
@@ -1,55 +1,50 @@
 'use client';
-import { useCloseModal } from "@/components/modal/modal-context";
-import React from "react";
-import type { Organization } from "@prisma/client";
-import { useQuery } from "react-query";
-import Dialog from "@/components/dialog";
+import React from 'react';
+import type {Organization} from '@prisma/client';
+import {useCloseModal} from '@/components/modal/modal-context.ts';
+import Dialog from '@/components/dialog.tsx';
 import Button from '@/components/button/button.tsx';
-import LoadingSpinner from "@/components/loading-spinner";
-
+import LoadingSpinner from '@/components/loading-spinner.tsx';
 
 export type OrganizationDeletionDialogProps = {
-    readonly organizationId: number;
-}
+	readonly deleteOrganization: () => void;
+	readonly organization: Organization;
+};
 
 export default function OrganizationDeletionDialog(props: OrganizationDeletionDialogProps) {
-    const {organizationId} = props;
-    const closeModal = useCloseModal();
-    const {data} = useQuery<Organization[]>([organizationId, 'organizations'], async () => {
-        const response = await fetch(`/api/users/${organizationId}/dependant-organizations`);
-        return response.json();
-    });
+	const {deleteOrganization, organization} = props;
+	const closeModal = useCloseModal();
 
-    return (
-        <Dialog title={<span className="text-red-400">Borrar la organización</span>}>
-            ¿Estás seguro de que quieres borrar la organización?
-            {data 
-                ? (
-                    <div className="mt-4">
-                        {data.length === 1 && 'Se borrara la siguiente organizacion:'}
-                        <ul className="list-disc list-inside">
-                            {
-                                data.map(organization => (
-                                    <li key={organization.id}>
-                                        {organization.name}
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-                )
-                : (
-                    <div className="h-16 flex items-center justify-center mt-4">
-                        <LoadingSpinner/>
-                    </div>
-                )
-                }
-            <div className="mt-4 flex justify-between">
-                <Button variant="secondary" onPress={closeModal}>
-                    Cancelar
-                </Button>
-            </div>
-        </Dialog>
-    )
+	return (
+		<Dialog title={<span className='text-red-400'>Borrar la organización</span>}>
+			¿Estás seguro de que quieres borrar la organización?
+			<div className='mt-4'>
+				Se borrará la siguiente organización:
+				{
+					organization
+						? <ul className='list-disc list-inside'>
+							<li key={organization.id}>
+								{organization.name}
+							</li>
+						</ul>
+						: <div className='h-16 flex items-center justify-center mt-4'>
+							<LoadingSpinner/>
+						</div>
+				}
+			</div>
+			<div className='mt-4 flex justify-between'>
+				<Button variant='secondary' onPress={closeModal}>
+					Cancelar
+				</Button>
+				<Button
+					variant='outlined' className='text-red-400 font-bold' onPress={async () => {
+						await deleteOrganization();
+						window.location.href = '/my';
+					}}>
+					Confirmar
+				</Button>
+			</div>
+		</Dialog>
+	);
 }
 
