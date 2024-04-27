27f08ae9bc6fc2b7824e5f596ad4e3e305268fe6
Hiram Muñoz <hirammunozr@outlook.com>
2024-04-27 12:33:42 -0600

    Installed turbo

---
+++ packages/eslint/eslint.config.js
@@ -0,0 +1,60 @@
+import {
+	FlatCompat
+} from '@eslint/eslintrc';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginUnicorn from 'eslint-plugin-unicorn';
+import pluginTailwindcss from 'eslint-plugin-tailwindcss';
+import pluginSonarJs from 'eslint-plugin-sonarjs';
+import pluginSecurity from 'eslint-plugin-security';
+import pluginReactRedux from 'eslint-plugin-react-redux';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+baseDirectory: __dirname, // optional; default: process.cwd()
+resolvePluginsRelativeTo: __dirname, // optional
+recommendedConfig: pluginJs.configs.recommended, // optional unless you're using "eslint:recommended"
+allConfig: pluginJs.configs.all, // optional unless you're using "eslint:all"
+});
+
+export default [
+{languageOptions: {globals: globals.browser}},
+pluginJs.configs.recommended,
+...tseslint.configs.recommended,
+pluginUnicorn.configs['flat/recommended'],
+...compat.config(pluginTailwindcss.configs.recommended),
+pluginSecurity.configs.recommended,
+...compat.config(pluginReactRedux.configs.recommended),
+pluginReactConfig,
+...compat.config(pluginSonarJs.configs.recommended),
+...compat.config(pluginReactHooks.configs.recommended),
+eslintPluginPrettierRecommended,
+{
+rules: {
+'unicorn/prevent-abbreviations': [
+'error',
+{
+replacements: {
+props: false,
+ref: false,
+args: false,
+},
+},
+],
+'react-redux/no-unused-prop-types': 'off', // weird duplicate rule that causes false positives
+'react/react-in-jsx-scope': 'off',
+},
+},
+{
+ignores: ['src/routeTree.gen.ts'],
+},
+];
